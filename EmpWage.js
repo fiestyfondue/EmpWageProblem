1f2a24d2dca0c9efe22809bbbb13af04e5ba7adf
Harsh <harsh.21483@gmail.com>
2021-07-15 08:20:51 -0700

    [Harsh]Refactor:- MOnthly wage is added

--- EmpWage.js
+++ EmpWage.js
@@ -1,31 +1,33 @@
-//constants
-const PART_TIME = 4;
-const FULL_TIME = 8;
-const WAGE_PER_HR = 20;
-const FULL_TIME_WORK = 1;
-const HALF_TIME_WORK = 2;
-//variables
-let empWage , workingHours =0;
-let workingDays = 30;
+const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
-while (workingDays != 0){
-    let empCheck = Math.floor(Math.random() * 3 );
-    workingHours += GetHours(empCheck);
-    workingDays--;
+// function getworkinghours( parameter)
+function GetWorkingHours(empCheck){
+switch (empCheck) {
+    case IS_ABSENT:  
+       return 0; 
+        break;
+    case IS_PART_TIME:
+        return PART_TIME_HOURS; 
+        break;
+    case IS_FULL_TIME: 
+        return FULL_TIME_HOURS; 
+        break;
+    default:
+        return  null; 
 }
-empWage = workingHours * WAGE_PER_HR;
-console.log("Employee Wage: " + empWage);
-//function to get emphours
- function GetHours(empCheck){
-    switch(empCheck){
-        case FULL_TIME_work:
-            //employee is present and full time
-            return  FULL_TIME;
-        case HALF_TIME_WORK:
-            //employee is present and part-time
-            return PART_TIME;   
-        default:
-            //employee is absent
-            return 0;
-    }
 }
+
+let empHrs = 0;
+const WORKING_DAYS_IN_A_MONTH = 20;
+for (let days = 1; days <= WORKING_DAYS_IN_A_MONTH; days++) {
+let empCheck = Math.floor(Math.random() * 3) ;   //Gives values anywhere from 0,1,2
+    empHrs += GetWorkingHours(empCheck);  // empHrs=empHrs+ empCheck random value
+}
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total Hours: " + empHrs + "\nTotal Wage: " + empWage);
